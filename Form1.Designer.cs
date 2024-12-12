3f857e3dd3e16338e2f0bb4d01057822f76c6e94
Mustafa Sezer <156379819+Elanzeon@users.noreply.github.com>
2024-12-12 21:11:42 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace NewsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstNews = new System.Windows.Forms.ListBox();
+            this.btnFetchNews = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstNews
+            // 
+            this.lstNews.FormattingEnabled = true;
+            this.lstNews.Location = new System.Drawing.Point(132, 73);
+            this.lstNews.Name = "lstNews";
+            this.lstNews.Size = new System.Drawing.Size(510, 251);
+            this.lstNews.TabIndex = 0;
+            this.lstNews.Click += new System.EventHandler(this.btnFetchNews_Click);
+            this.lstNews.SelectedIndexChanged += new System.EventHandler(this.lstNews_SelectedIndexChanged);
+            // 
+            // btnFetchNews
+            // 
+            this.btnFetchNews.Location = new System.Drawing.Point(39, 286);
+            this.btnFetchNews.Name = "btnFetchNews";
+            this.btnFetchNews.Size = new System.Drawing.Size(69, 38);
+            this.btnFetchNews.TabIndex = 1;
+            this.btnFetchNews.Text = "Haberleri Getir";
+            this.btnFetchNews.UseVisualStyleBackColor = true;
+            this.btnFetchNews.Click += new System.EventHandler(this.btnFetchNews_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.BackColor = System.Drawing.SystemColors.Control;
+            this.lblError.Location = new System.Drawing.Point(12, 367);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 13);
+            this.lblError.TabIndex = 2;
+            this.lblError.Click += new System.EventHandler(this.lblError_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btnFetchNews);
+            this.Controls.Add(this.lstNews);
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Name = "MainForm";
+            this.Text = "Haber Sayfası";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstNews;
+        private System.Windows.Forms.Button btnFetchNews;
+        private System.Windows.Forms.Label lblError;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace NewsApp
+{
+    public partial class MainForm : Form
+    {
+        // API anahtarınız ve endpoint URL'si
+        private const string apiKey = "f8f67dbc7740ee4753860db873db8f9d"; // Buraya kendi API anahtarınızı yazın
+        private const string apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=" + apiKey;
+
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // Butona tıklama olayı
+        private async void btnFetchNews_Click(object sender, EventArgs e)
+        {
+            await FetchNewsAsync();
+        }
+
+        // API'den haber çekme metodu
+        private async Task FetchNewsAsync()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", "Bearer " + apiKey);
+                    // API'ye GET isteği gönder
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+                    response.EnsureSuccessStatusCode(); // Başarısız cevap durumlarını kontrol et
+
+                    string jsonString = await response.Content.ReadAsStringAsync();
+                    lblError.Text = $"Hata: {response.StatusCode} - {response.ReasonPhrase}\n{jsonString}";
+
+                    MessageBox.Show(jsonString, "API Yanıtı");
+                    // JSON verisini C# nesnesine dönüştür
+                    var newsData = JsonConvert.DeserializeObject<NewsResponse>(jsonString);
+
+                    // ListBox'a haber başlıklarını ekle
+                    lstNews.Items.Clear(); // Önceki verileri temizle
+                    foreach (var article in newsData.articles)
+                    {
+                        lstNews.Items.Add(article.title); // Sadece haber başlığını ekle
+                        if (!string.IsNullOrEmpty(article.title))
+                        {
+                            lstNews.Items.Add(article.title);
+                        }
+                        else
+                        {
+                            lstNews.Items.Add("Başlık bulunamadı.");
+                        }
+                    }
+                }
+            }
+            catch (HttpRequestException httpEx)
+            {
+                // HTTP hatalarını burada yakala ve ekrana yazdır
+                lblError.Text = $"Hata: {httpEx.Message}";
+            }
+            catch (Exception ex)
+            {
+                // Diğer genel hataları burada yakala ve ekrana yazdır
+                lblError.Text = $"Genel hata: {ex.Message}";
+            }
+
+        // ListBox'tan bir haber seçildiğinde detaylarını göster
+        private void lstNews_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstNews.SelectedIndex != -1) // Eğer bir öğe seçildiyse
+            {
+                var selectedArticle = ((NewsResponse)lstNews.Tag).articles[lstNews.SelectedIndex];
+                MessageBox.Show($"Başlık: {selectedArticle.title}\n\n" +
+                                $"Açıklama: {selectedArticle.description}\n\n" +
+                                $"URL: {selectedArticle.url}", "Haber Detayları", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void lblError_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    // JSON verisini karşılayacak model sınıfı
+    public class NewsResponse
+    {
+        public string status { get; set; }
+        public int totalResults { get; set; }
+        public Article[] articles { get; set; }
+    }
+
+    public class Article
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string url { get; set; }
+    }
+}
